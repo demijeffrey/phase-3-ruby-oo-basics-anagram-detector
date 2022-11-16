574f7b811b18ddcbb149ac289cd883da06190744
Demaris Ashmore <demiashmore@yahoo.com>
2022-11-16 15:29:55 -0700

    commit tree e403e316bce5f85a2a9cdb6d0cd66a130e8f6e8d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        # new_array = %w[array]
+        array.filter do |str|
+            str.chars.sort == word.chars.sort
+        end
+    end
+end
